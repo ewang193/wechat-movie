e0631ae4dfe43c4674cb6e830c4db5448e650b1b
elina66 <elinawang66@126.com>
2017-11-13 09:39:52 +0800

    change appjs

--- app.js
+++ app.js
@@ -2,35 +2,47 @@
 
 var koa = require('koa');
 var sha1 = require('sha1');
-// var route = require('koa-route');
-var config = {
-    wechat: {
-        appID: 'wx1ba5f1d5e4960b1e',
-        appSecret: '430bdc6a070e50b6cf34cbd38a39b691',
-        token: 'testtoken'   //自己设置的token值
-    }
-};
+var route = require('koa-route');
+// var config = {
+//     wechat: {
+//         appID: 'wx1ba5f1d5e4960b1e',
+//         appSecret: '430bdc6a070e50b6cf34cbd38a39b691',
+//         token: 'testtoken'   //自己设置的token值
+//     }
+// };
 
 var app = new koa();   //实例化一个koa的web服务器
 
-app.use(function *(next) {    // *表示是生成器函数，KOA也只接受生成器函数，generator function
-    console.log(this.query);
+// app.use(function *(next) {    // *表示是生成器函数，KOA也只接受生成器函数，generator function
+//     console.log(this.query);
+//
+//     var token = config.wechat.token;
+//     var signature = this.query.signature;
+//     var nonce = this.query.nonce;
+//     var timestamp = this.query.timestamp;
+//     var echostr = this.query.echostr;
+//
+//     var str = [token, timestamp, nonce].sort().join('');
+//     var sha = sha1(str);
+//
+//     if(sha === signature){
+//         this.body = echostr + '';
+//     } else {
+//         this.body = 'wrong';
+//     }
+// });
 
-    var token = config.wechat.token;
-    var signature = this.query.signature;
-    var nonce = this.query.nonce;
-    var timestamp = this.query.timestamp;
-    var echostr = this.query.echostr;
+function about(ctx){
+    ctx.response.type = "html";
+    ctx.response.body = '<a href="/">Index Page</a>';
+}
 
-    var str = [token, timestamp, nonce].sort().join('');
-    var sha = sha1(str);
+function main(ctx){
+    ctx.response.body = 'Hello World';
+}
 
-    if(sha === signature){
-        this.body = echostr + '';
-    } else {
-        this.body = 'wrong';
-    }
-});
+app.use(route.get('/', main));
+app.use(route.get('/about', about));
 
 app.listen(1234);
 console.log('Listening: 1234');
@@ -42,14 +54,7 @@ console.log('Listening: 1234');
 
 
 
-// function about(ctx){
-//     ctx.response.type = "html";
-//     ctx.response.body = '<a href="/">Index Page</a>';
-// }
-//
-// function main(ctx){
-//     ctx.response.body = 'Hello World';
-// }
+
 
 // const about = ctx => {
 //     ctx.response.type = "html";
@@ -60,5 +65,4 @@ console.log('Listening: 1234');
 //     ctx.response.body = 'Hello World';
 // };
 
-// app.use(route.get('/', main));
-// app.use(route.get('/about', about));
+
