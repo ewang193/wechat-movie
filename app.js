f163beb2669ac18692e601634cf5fea8f70ed095
elina66 <elinawang66@126.com>
2017-11-15 16:01:47 +0800

    use access_token

--- app.js
+++ app.js
@@ -38,13 +38,22 @@
 'use strict';
 
 var koa = require('koa');
-var sha1 = require('sha1');
+var path = require('path');
 var wechat = require('./wechat/g');
+var util = require('./libs/util');
+var wechat_file = path.join(__dirname, './config/wechat.txt');
 var config = {
     wechat: {
         appID: 'wx1ba5f1d5e4960b1e',
         appSecret: '430bdc6a070e50b6cf34cbd38a39b691',
-        token: 'testtoken'
+        token: 'testtoken',
+        getAccessToken: function(){
+            return util.readFileAsync(wechat_file);
+        },
+        saveAccessToken: function(data) {
+            data = JSON.stringify(data);
+            return util.writeFileAsync(wechat_file, data);
+        }
     }
 };
 
--- libs/util.js
+++ libs/util.js
@@ -1,3 +1,28 @@
-/**
- * Created by ewang193 on 2017/11/14.
- */
+'use strict';
+
+var fs = require('fs');
+var Promise = require('bluebird');
+
+exports.readFileAsync = function(fpath, encoding) {
+    return new Promise(function(resolve, reject){
+        fs.readFile(fpath, encoding, function(err, content){
+            if(err){
+                reject(err);
+            } else {
+                resolve(content);
+            }
+        })
+    })
+};
+
+exports.writeFileAsync = function(fpath, cotent) {
+    return new Promise(function(resolve, reject){
+        fs.writeFile(fpath, content, function(err){
+            if(err){
+                reject(err);
+            } else {
+                resolve(content);
+            }
+        })
+    })
+};
